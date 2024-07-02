a56b53a37bc4677ee89e11fd894079c433008e05
Rafael Damasceno <rafaeldamascenowhy@gmail.com>
2024-07-02 02:46:10 -0300

    :sparkles: feat: Header e footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,5 +1,11 @@
 import './Footer.css'
 import Logo from '../../assets/logo.png'
+import seta from '../../assets/seta.svg'
+import map from '../../assets/map.svg'
+import Cell from '../../assets/CellIcon.svg'
+import Whats from '../../assets/WhatsIcon.svg'
+import fb from '../../assets/fb.svg'
+import next from '../../assets/next.svg'
 
 const Footer = () => {
   
@@ -8,27 +14,48 @@ const Footer = () => {
       <>
       
       <div className="footer">
+        <div className="supa">
         <div className="box">
             <div className="container_grid">
                 <div className="item1">
-                    <img src={Logo} alt="" />
-                    <span><img src='' alt="" /><p>Rodas e Motores</p></span>
-                    <span><img src="" alt="" /><p>Fale Conosco</p></span>
-                    <span><img src="" alt="" /><p>Av. Fernando Correa da Costa, 400, Sala 05 - Poção</p></span>
-                    <span><img src="" alt="" /><p>Av. Historiador Rubens de Mendonça, 553 - Araés</p></span>
-                    <span><img src="" alt="" /><p>Cuiabá - Mato Grosso</p></span>
+                    <img src={Logo} alt=""  className='item1_logo'/>
                     <ul>
-                        <li><img src="" alt="" /><a href="">(00)00000-0000</a></li>
-                        <li><img src="" alt="" /><a href="">(00)00000-0000</a></li>
+                        <li><img src={seta} alt="" /><a href="">Rodas e Motores</a></li>
+                        <li><img src={seta} alt="" /><a href="">Fale Conosco</a></li>
+                        <li><img src={map} alt="" /><a href="">Av. Fernando Correa da Costa, 400, Sala 05 - Poção</a></li>
+                        <li><img src={map} alt="" /><a href="">Av. Historiador Rubens de Mendonça, 553 - Araés</a></li>
+                        <li><img src={map} alt="" /><a href="">Cuiabá - Mato Grosso</a></li>
+                        <li><img src={Cell} alt="" /><a href="">(00)00000-0000</a></li>
+                        <li><img src={Whats} alt="" /><a href="">(00)00000-0000</a></li>
                     </ul>
                 </div>
                 <div className="item2">
                     <div className="anuncio">
-
+                            <div className="box_perfil">
+                                <div className="perfil"></div>
+                                <div className="text_perfil">
+                                    <ul>
+                                        <li>Anúncio</li>
+                                        <li>29.299 seguidores</li>
+                                    </ul>
+                                    
+                                </div>
+                                
+                                
+                            </div>
+                            <div className="buttons_end">
+                                <button> <img src={fb} alt="" />Seguir Página</button>
+                                <button><img src={next} alt="" />Compartilhar</button>
+                            </div>
                     </div>
                 </div>
             </div>
         </div>
+        </div>
+        <div className="end">
+            <p>© 2024 Synergy4tech -  Todos os direitos reservados  |  Política de Privacidade</p>
+        </div>
+
       </div>
      
           
