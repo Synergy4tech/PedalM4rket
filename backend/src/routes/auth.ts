fab42b65a8b596a8c797e4a463b6c5bdf9651e39
Tulio Leal <denki_i@icloud.com>
2024-07-06 13:49:39 -0300

    🧹 cleanup: backend removido; 🚚 chore: component movido para homepage

--- backend/src/routes/auth.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { register, login } from "../models/User"
-
-const router = Router();
-
-router.post("/register", register);
-router.post("/login", login);
-
-export default router;
